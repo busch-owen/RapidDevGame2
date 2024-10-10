0703f08dffe351d92b7a3dcebe856794b61cbae1
busch-owen <buschowen@gmail.com>
2024-10-10 12:15:43 -0600

    Fixed some performance stuff with the mouse cursor

--- Assets/Scripts/GridSystem/GridPointer.cs
+++ Assets/Scripts/GridSystem/GridPointer.cs
@@ -12,9 +12,10 @@ public class GridPointer : MonoBehaviour
     private LayerMask _gridLayer;
 
     [SerializeField] private Grid grid;
-
-    [SerializeField] private GameObject debugPointer;
+    
     [SerializeField] private GameObject gridPointer;
+    
+    public Vector3Int CurrentCellPos { get; private set; }
 
     private void Start()
     {
@@ -22,19 +23,25 @@ public class GridPointer : MonoBehaviour
         _gridLayer = LayerMask.GetMask("Grid");
     }
 
-    private void Update()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(_camera.ScreenToWorldPoint(_mousePos), _camera.transform.forward, _gridLayer);
-        if (hit)
-        {
-            debugPointer.transform.position = hit.point;
-            var cellPos = grid.WorldToCell(hit.point);
-            gridPointer.transform.position = cellPos;
-        }
-    }
-
     public void ChangePointerPosition(Vector2 position)
     {
         _mousePos = position;
+        RaycastHit2D hit = Physics2D.Raycast(_camera.ScreenToWorldPoint(_mousePos), _camera.transform.forward, _gridLayer);
+        if (hit)
+        {
+            if (!gridPointer.activeSelf)
+            {
+                gridPointer.SetActive(true);
+            }
+            CurrentCellPos = grid.WorldToCell(hit.point);
+            gridPointer.transform.position = CurrentCellPos;
+        }
+        else
+        {
+            if (gridPointer.activeSelf)
+            {
+                gridPointer.SetActive(false);
+            }
+        }
     }
 }
