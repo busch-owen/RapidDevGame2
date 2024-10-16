ede684fdc7c73ba0757ab4ad2a886252d65da09d
busch-owen <buschowen@gmail.com>
2024-10-16 11:30:14 -0600

    Created StoreObjectSO script

--- Assets/Scripts/Npcs/Targets/ItemTypeSo.cs
+++ Assets/Scripts/Npcs/Targets/ItemTypeSo.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ProductType
+{
+    Console, Game, Controller, Collectable, Apparel
+}
+
 [CreateAssetMenu(menuName = "My Assets/ItemTypeSo")]
 
 public class ItemTypeSo : ScriptableObject
