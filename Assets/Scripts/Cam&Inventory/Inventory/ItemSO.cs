0970e57688b099e62946cb9c0fc0233323a3e306
IamNomadic <xr3thx@gmail.com>
2024-10-19 21:33:24 -0600

    minor cleanup

--- Assets/Scripts/Cam&Inventory/Inventory/ItemSO.cs
+++ Assets/Scripts/Cam&Inventory/Inventory/ItemSO.cs
@@ -8,6 +8,7 @@ public class ItemSO : ScriptableObject
     [SerializeField] public string itemName;
     [SerializeField] public string itemDescription;
     [SerializeField] public string itemCategory;
+    [SerializeField] public int itemPrice;
     [SerializeField] public Sprite bigIcon;
     [SerializeField] public Sprite smallIcon;
 
