a4baaa3521aa22e4f2abed9bad737521e436dcef
Ayden <Theownage79@gmail.com>
2024-10-21 14:54:10 -0600

    Npc wave spawning functional

--- Assets/Scripts/Npcs/systems/NpcSpawner.cs
+++ Assets/Scripts/Npcs/systems/NpcSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
@@ -10,11 +11,19 @@ public class NpcSpawner : MonoBehaviour
 
     private NPCAI _npcai;
 
+    private WaitForSeconds _timeToNextWave;
+
+    [SerializeField] private float time;
+
     [SerializeField] private Transform entrance;
     
     [field: SerializeField] public int Entrances { get; private set; }
     
     [field: SerializeField] public List<GameObject> Cars{ get; private set; } = new();
+    
+    [field: SerializeField] public List<int> Waves{ get; private set; } = new();
+    
+    [field: SerializeField] public List<int> AmountOfNpcs{ get; private set; } = new();
 
 
     public void SpawnNpc()
@@ -25,5 +34,33 @@ public class NpcSpawner : MonoBehaviour
 
         entrance = Cars[Entrances].transform;
         var spawnedNpc = Instantiate(npc, entrance.transform.position, quaternion.identity);
+    }
+
+    public void StartDay()
+    {
+        StartCoroutine("NpcWaveSpawn");
+    }
+
+    public void EndDay()
+    {
+        StopCoroutine("NpcWaveSpawn");
+    }
+
+    public IEnumerator NpcWaveSpawn()
+    {
+        foreach (int amtofWaves in Waves)
+        {
+            foreach (int amtofNpcs in AmountOfNpcs)
+            {
+                SpawnNpc();
+            }
+            yield return _timeToNextWave;
+        }
+        
+    }
+
+    private void Start()
+    {
+        _timeToNextWave = new WaitForSeconds(time);
     }
 }
