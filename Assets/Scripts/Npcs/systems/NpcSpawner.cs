69476b1de530111d6d400499e25333ef4b878029
Ayden <Theownage79@gmail.com>
2024-10-21 15:26:19 -0600

    waves now randomize amount of npcs spawned between min and max range

--- Assets/Scripts/Npcs/systems/NpcSpawner.cs
+++ Assets/Scripts/Npcs/systems/NpcSpawner.cs
@@ -9,8 +9,6 @@ public class NpcSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject npc;
 
-    private NPCAI _npcai;
-
     private WaitForSeconds _timeToNextWave;
 
     [SerializeField] private float time;
@@ -24,12 +22,18 @@ public class NpcSpawner : MonoBehaviour
     [field: SerializeField] public List<int> Waves{ get; private set; } = new();
     
     [field: SerializeField] public List<int> AmountOfNpcs{ get; private set; } = new();
+    
+    [field: SerializeField] public int MaxNpcs { get;  set; }
+    
+    [field: SerializeField] public int MinNpcs { get;  set; }
+    
+    private int _npcsToSpawn;
+
+    private bool _running;
 
 
     public void SpawnNpc()
     {
-        _npcai = npc.GetComponent<NPCAI>();
-        
         Entrances = Random.Range(0, Cars.Count);
 
         entrance = Cars[Entrances].transform;
@@ -38,23 +42,50 @@ public class NpcSpawner : MonoBehaviour
 
     public void StartDay()
     {
-        StartCoroutine("NpcWaveSpawn");
+        StartCoroutine("RandomizeNpcs");
     }
 
     public void EndDay()
     {
         StopCoroutine("NpcWaveSpawn");
+        
     }
 
     public IEnumerator NpcWaveSpawn()
     {
         foreach (int amtofWaves in Waves)
         {
+            
             foreach (int amtofNpcs in AmountOfNpcs)
             {
                 SpawnNpc();
             }
+            Invoke("Randomize", time -1);
             yield return _timeToNextWave;
+        }
+    }
+
+    private void Randomize()
+    {
+        StartCoroutine("RandomizeNpcs");
+    }
+
+    private IEnumerator RandomizeNpcs()
+    {
+        _npcsToSpawn = Random.Range(MinNpcs, MaxNpcs);
+        for (int i = 0; AmountOfNpcs.Count <= _npcsToSpawn; i++)
+        {
+            AmountOfNpcs.Add(i);
+        }
+
+        if (!_running)
+        {
+            _running = true;
+            yield return StartCoroutine("NpcWaveSpawn");
+        }
+        else
+        {
+            yield return null;
         }
         
     }
